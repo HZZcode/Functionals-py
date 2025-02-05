96fbe8debb4e39849463ca1b09aeede52f4c8985
ZZ H <HZZcode@outlook.com>
2025-02-05 11:06:23 +0800

    typing C for chain

--- Chain.py
+++ Chain.py
@@ -1,11 +1,11 @@
 from typing import TypeVar, Callable, Any
 
 C = TypeVar('C', bound = Any)
-T = TypeVar('T', bound = Callable[[C], Any])
+T = TypeVar('T', bound = Callable[[C], C])
 
 def Chain(times: int) -> Callable[[T], T]:
     def chainer(f: T) -> T:
-        def f_chain(arg):
+        def f_chain(arg: C) -> C:
             ans = arg
             for i in range(times):
                 ans = f(ans)
