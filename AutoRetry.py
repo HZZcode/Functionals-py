a6c6916b0cc25539d831b5129e9fd294216f82f9
ZZ H <HZZcode@outlook.com>
2025-08-31 15:52:06 +0800

    fix auto-retry and auto-sum

--- AutoRetry.py
+++ AutoRetry.py
@@ -1,25 +1,25 @@
-import itertools
 from functools import wraps
+from itertools import count
 from time import sleep
-from typing import Callable, Iterable
+from typing import Callable
 
 
-def AutoRetry(except_type: type = Exception, max_retry: int | None = None,
-              info_interval: int = 1, retry_delay: float = 0):
+def AutoRetry(except_type: type = Exception, /, max_retry: int | None = None,
+              info_interval: int = 1, show_info: bool = True, retry_delay: float = 0):
     def retry[F: Callable](f: F) -> F:
-        it: Iterable
-        if max_retry is None:
-            it = itertools.count(start=0)
-
         @wraps(f)
         def f_retry(*args, **kwargs):
-            for retry_count in it:
+            exception = None
+            for retry_count in count(start=0) if max_retry is None else range(max_retry):
                 try:
                     return f(*args, **kwargs)
-                except except_type:
-                    if retry_count % info_interval == 0:
+                except except_type as e:
+                    exception = e
+                    if show_info and retry_count % info_interval == 0:
                         print(f'No.{retry_count + 1} retry of {f.__name__}')
                     sleep(retry_delay)
+            if exception is not None:
+                raise exception
 
         return f_retry
 
--- AutoSum.py
+++ AutoSum.py
@@ -2,8 +2,6 @@ from typing import Iterable
 
 
 def auto_sum[T](iterable: Iterable[T]) -> T:
-    first = next(iter(iterable), None)
-    if first is not None:
-        return sum(iterable, type(first)())
-    return sum(iterable)
-# BUG: auto_sum(Iterator) ignores the first element
+    iterator = iter(iterable)
+    first = next(iterator, None)
+    return sum(iterator, first) if first is not None else None
